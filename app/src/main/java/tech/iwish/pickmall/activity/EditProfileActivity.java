2c424629d7fbadb8722b815910fc0a6fd3afd02c
rr3343500 <rr3343500@gmail.com>
2020-06-20 14:00:18 +0530

    AddressActivity           complete

--- app/src/main/java/tech/iwish/pickmall/activity/EditProfileActivity.java
+++ app/src/main/java/tech/iwish/pickmall/activity/EditProfileActivity.java
@@ -83,7 +83,7 @@ public class EditProfileActivity extends AppCompatActivity implements AdapterVie
                    SaveProfileData(share_session.getUserDetail().get("UserMobile"),intent.getStringExtra("action"),gender);
                    share_session.UpdateUserDetail(gender,intent.getStringExtra("action"));
                    startActivity(new Intent(EditProfileActivity.this,Profile.class) );
-                   Animatoo.animateDiagonal(EditProfileActivity.this);
+//                   Animatoo.animateDiagonal(EditProfileActivity.this);
                }
            }
        });
